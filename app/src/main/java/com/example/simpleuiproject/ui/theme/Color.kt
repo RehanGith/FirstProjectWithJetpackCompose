04fc62529ea3b374fcb5332e232f8c8f7e27aedc
RehanGith <chrehan1007@gmail.com>
2025-03-05 23:08:14 +0500

    added the colors

--- app/src/main/java/com/example/simpleuiproject/ui/theme/Color.kt
+++ app/src/main/java/com/example/simpleuiproject/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Pink200 = Color(0xfff48fb1)
+val Pink500 = Color(0xfff06292)
+val Purple500 = Color(0xff6200ee)
+val Purple300 = Color(0xff9575cd)
