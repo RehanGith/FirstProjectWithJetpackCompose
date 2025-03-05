365d284fb57d87537dcb9645db45ed6cb7ef0598
RehanGith <chrehan1007@gmail.com>
2025-03-06 01:03:54 +0500

    Added the drawables and created a card layout

--- app/src/main/java/com/example/simpleuiproject/HomeUI.kt
+++ app/src/main/java/com/example/simpleuiproject/HomeUI.kt
@@ -1,6 +1,7 @@
 package com.example.simpleuiproject
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -11,14 +12,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -46,15 +55,8 @@ fun HomeUi() {
                     Buttons(icon1 = R.drawable.baseline_search_24, tint = Color.White)
                 }
 
-                LazyColumn (
-                    content = {
-                        item {
-
-                        }
-                    }
-                )
-
             }
+            MyCard()
         }
     }
 }
@@ -71,5 +73,23 @@ fun Buttons(
 }
 @Composable
 fun MyCard() {
+    Card(shape = RoundedCornerShape(10.dp)
+    , modifier = Modifier
+            .height(175.dp)
+            .width(200.dp)
+            .padding(10.dp)
+            .background(Color.Gray)){
+        Box(modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+                Image(painter = painterResource(id = R.drawable.profile), contentDescription = null, modifier = Modifier
+                    .clip(CircleShape)
+                    .size(100.dp),
+                    contentScale = ContentScale.Crop)
 
+            }
+
+        }
+
+    }
 }
